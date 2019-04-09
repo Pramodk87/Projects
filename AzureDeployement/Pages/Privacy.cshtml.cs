27cddb506db71394d663bc4b68b34018259a70c2
Pramod Kumar <PramodKum@hcl.com>
2019-04-09 11:51:46 +0530

    code commit for git repo

--- AzureDeployement/Pages/Privacy.cshtml.cs
+++ AzureDeployement/Pages/Privacy.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace GitRepoDeployment.Pages
+namespace AzureDeployement.Pages
 {
     public class PrivacyModel : PageModel
     {
--- AzureDeployement/Startup.cs
+++ AzureDeployement/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace GitRepoDeployment
+namespace AzureDeployement
 {
     public class Startup
     {
